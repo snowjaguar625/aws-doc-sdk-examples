b92bd86e579e80a1ee57618f146a5f7e52c9b296
snowjaguar625 <snowjaguar625@gmail.com>
2016-12-15 17:13:50 -0800

    Add S3 Go Bucket Website Configuration examples

--- python/example_code/s3/s3-python-example-get-bucket-cors.py
+++ python/example_code/s3/s3-python-example-get-bucket-cors.py
@@ -17,5 +17,4 @@ import boto3
 s3 = boto3.client('s3')
 
 # Call S3 to retrieve CORS configuration for selected bucket
-result = s3.get_bucket_cors(Bucket='my-bucket')
-
+s3.get_bucket_cors(Bucket='my-bucket')
