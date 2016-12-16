1ef9cb3341c0d477bc1fd10436a27945474c3ad6
snowjaguar625 <snowjaguar625@gmail.com>
2016-12-16 10:52:00 -0800

    Add copyright headers and minor typo fixes in comments.

--- go/example_code/s3/s3_get_bucket_acl.go
+++ go/example_code/s3/s3_get_bucket_acl.go
@@ -17,50 +17,52 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// Creates an S3 Bucket in the region configured in the shared config
-// or AWS_REGION environment variable.
+// Requests the ACL of an S3 Bucket and prints it out.
 //
 // Usage:
-//    go run s3_create_bucket BUCKET_NAME
+//    go run s3_get_bucket_acl.go BUCKET_NAME
 func main() {
 	if len(os.Args) != 2 {
-		exitErrorf("bucket name required\nUsage: %s bucket_name", os.Args[0])
+		exitErrorf("Bucket name required\nUsage: %s bucket_name",
+			filepath.Base(os.Args[0]))
 	}
 	bucket := os.Args[1]
 
-	// Initialize a session that the SDK will use to load configuration,
+	// Inititalize a session that the SDK will use to load configuration,
 	// credentials, and region from the shared config file. (~/.aws/config).
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
-	// Create S3 service client
+	// Create an S3 service client.
 	svc := s3.New(sess)
 
-	// Create the S3 Bucket
-	_, err := svc.CreateBucket(&s3.CreateBucketInput{
+	// Make the API request to S3 using the bucket name to get
+	// the bucket's ACL configuration.
+	result, err := svc.GetBucketAcl(&s3.GetBucketAclInput{
 		Bucket: aws.String(bucket),
 	})
 	if err != nil {
-		exitErrorf("Unable to create bucket %q, %v", bucket, err)
+		exitErrorf("Unable to get bucket %q ACL, %v", bucket, err)
 	}
 
-	// Wait until bucket is created before finishing
-	fmt.Printf("Waiting for bucket %q to be created...\n", bucket)
-	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
-		Bucket: aws.String(bucket),
-	})
-	if err != nil {
-		exitErrorf("Error occurred while waiting for bucket to be created, %v", bucket)
+	fmt.Printf("Bucket %q owned by %q\n", bucket,
+		aws.StringValue(result.Owner.DisplayName))
+	fmt.Println("Grants:")
+	for _, grant := range result.Grants {
+		fmt.Printf("* name: %s, type: %s, permission: %s\n",
+			aws.StringValue(grant.Grantee.DisplayName),
+			aws.StringValue(grant.Grantee.Type),
+			aws.StringValue(grant.Permission),
+		)
 	}
-
-	fmt.Printf("Bucket %q successfully created\n", bucket)
 }
 
 func exitErrorf(msg string, args ...interface{}) {
