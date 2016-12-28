a74637a43ddda11eab044d9066d351efd7e5d42f
snowjaguar625 <snowjaguar625@gmail.com>
2016-12-28 10:57:53 -0800

    Adding example for setting attributes on queue

--- go/example_code/sqs/sqs_deadletterqueue.go
+++ go/example_code/sqs/sqs_deadletterqueue.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -9,7 +10,7 @@ import (
 )
 
 // Usage:
-// go run sqs_createqueues.go
+// go run sqs_deadletterqueue.go
 func main() {
 	// Initialize a session that the SDK will use to load configuration,
 	// credentials, and region from the shared config file. (~/.aws/config).
@@ -20,17 +21,31 @@ func main() {
 	// Create a SQS service client.
 	svc := sqs.New(sess)
 
-	result, err := svc.CreateQueue(&sqs.CreateQueueInput{
-		QueueName: aws.String("SQS_QUEUE_NAME"),
+	// Our redrive policy for our queue
+	policy := map[string]string{
+		"deadLetterTargetArn": "SQS_QUEUE_ARN",
+		"maxReceiveCount":     "10",
+	}
+
+	// Marshal our policy to be used as input for our SetQueueAttributes
+	// call.
+	b, err := json.Marshal(policy)
+	if err != nil {
+		fmt.Println("Failed to marshal policy:", err)
+		return
+	}
+
+	result, err := svc.SetQueueAttributes(&sqs.SetQueueAttributesInput{
+		QueueUrl: aws.String("SQS_QUEUE_URL"),
 		Attributes: map[string]*string{
-			"DelaySeconds":           aws.String("60"),
-			"MessageRetentionPeriod": aws.String("86400"),
+			sqs.QueueAttributeNameRedrivePolicy: aws.String(string(b)),
 		},
 	})
+
 	if err != nil {
 		fmt.Println("Error", err)
 		return
 	}
 
-	fmt.Println("Success", *result.QueueUrl)
+	fmt.Println("Success", result)
 }
