1f59f64ad30e8786492e8dc6ea3b55287e96e3dc
snowjaguar625 <snowjaguar625@gmail.com>
2016-12-28 15:18:06 -0800

    add copyright headers

---
+++ go/example_code/iam/iam_createuser.go
@@ -0,0 +1,62 @@
+/*
+   Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
+)
+
+// Usage:
+// go run iam_createuser.go <username>
+func main() {
+	// Initialize a session that the SDK will use to load configuration,
+	// credentials, and region from the shared config file. (~/.aws/config).
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create a IAM service client.
+	svc := iam.New(sess)
+
+	_, err := svc.GetUser(&iam.GetUserInput{
+		UserName: &os.Args[1],
+	})
+
+	// If we received an error and it is an `awserr.Error`, we want to ensure
+	// that it is something other than a `NoSuchEntity` error.
+	if awserr, ok := err.(awserr.Error); ok && awserr.Code() != "NoSuchEntity" {
+		fmt.Println("GetUser Error", err)
+		return
+	} else if err == nil { // If there is no error, that means the user exists.
+		fmt.Println(fmt.Sprintf("User %s already exists", os.Args[1]))
+		return
+	}
+
+	result, err := svc.CreateUser(&iam.CreateUserInput{
+		UserName: &os.Args[1],
+	})
+
+	if err != nil {
+		fmt.Println("CreateUser Error", err)
+		return
+	}
+
+	fmt.Println("Success", result)
+}
