7ffcdf5605ccfc12e575ddf9add77ce974b5329e
snowjaguar625 <snowjaguar625@gmail.com>
2017-02-10 16:02:39 -0800

    Add documentation for Ec2 Operations

--- cpp/example_code/s3/create_bucket.cpp
+++ cpp/example_code/s3/create_bucket.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
    This file is licensed under the Apache License, Version 2.0 (the "License").
    You may not use this file except in compliance with the License. A copy of
@@ -34,21 +34,20 @@ int main(int argc, char** argv)
     const Aws::String bucket_name = argv[1];
     std::cout << "Creating S3 bucket: " << bucket_name << std::endl;
 
-    {
-        Aws::S3::S3Client s3_client;
+    Aws::S3::S3Client s3_client;
 
-        Aws::S3::Model::CreateBucketRequest bucket_request;
-        bucket_request.WithBucket(bucket_name);
+    Aws::S3::Model::CreateBucketRequest bucket_request;
+    bucket_request.WithBucket(bucket_name);
 
-        auto create_bucket_outcome = s3_client.CreateBucket(bucket_request);
+    auto create_bucket_outcome = s3_client.CreateBucket(bucket_request);
 
-        if (create_bucket_outcome.IsSuccess()) {
-            std::cout << "Done!" << std::endl;
-        } else {
-            std::cout << "CreateBucket error: " <<
-                create_bucket_outcome.GetError().GetExceptionName() << std::endl
-                << create_bucket_outcome.GetError().GetMessage() << std::endl;
-        }
+    if(create_bucket_outcome.IsSuccess()) {
+        std::cout << "Done!" << std::endl;
+    }
+    else {
+        std::cout << "CreateBucket error: " <<
+            create_bucket_outcome.GetError().GetExceptionName() << std::endl <<
+            create_bucket_outcome.GetError().GetMessage() << std::endl;
     }
 
     Aws::ShutdownAPI(options);
