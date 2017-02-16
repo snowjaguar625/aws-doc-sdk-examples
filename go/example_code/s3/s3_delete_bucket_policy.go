d5349184bf5324893bb525230a3722321e3eb6dc
snowjaguar625 <snowjaguar625@gmail.com>
2017-02-16 11:57:56 -0800

    removing question marks from end of var name - not allowed in Ruby

---
+++ go/example_code/s3/s3_delete_bucket_policy.go
@@ -0,0 +1,68 @@
+/*
+   Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// Deletes the policy on a bucket. If the bucket doesn't exist, or there was
+// and error an error message will be printed instead.
+//
+// Usage:
+//    go run s3_delete_bucket_policy.go BUCKET_NAME
+func main() {
+	if len(os.Args) != 2 {
+		exitErrorf("bucket name required\nUsage: %s bucket_name",
+			filepath.Base(os.Args[0]))
+	}
+	bucket := os.Args[1]
+
+	// Initialize a session that the SDK will use to load configuration,
+	// credentials, and region from the shared config file. (~/.aws/config).
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	// Call S3 to delete the policy on the bucket.
+	_, err := svc.DeleteBucketPolicy(&s3.DeleteBucketPolicyInput{
+		Bucket: aws.String(bucket),
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == s3.ErrCodeNoSuchBucket {
+			// Special error handling for the when the bucket doesn't
+			// exists so we can give a more direct error message from the CLI.
+			exitErrorf("Bucket %q does not exist", bucket)
+		}
+		exitErrorf("Unable to delete bucket %q policy, %v", bucket, err)
+	}
+
+	fmt.Printf("Successfully deleted the policy on bucket %q.\n", bucket)
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
