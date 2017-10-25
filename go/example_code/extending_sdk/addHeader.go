86794cbc3ca4a73e3d784d4fa286ccfce52cd091
snowjaguar625 <snowjaguar625@gmail.com>
2017-10-25 14:30:33 -0700

    Added example of adding a custom header to a service call in Go

--- go/example_code/extending_sdk/addHeader.go
+++ go/example_code/extending_sdk/addHeader.go
@@ -16,36 +16,36 @@ package main
 
 import (
     "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/request"
     "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/cloudtrail"
-    
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+
     "fmt"
     "os"
 )
 
 func main() {
-    // Initialize a session in us-west-2 that the SDK will use to load
-    // credentials from the shared credentials file ~/.aws/credentials.
+    // Initialize a session in us-west-2 that the SDK will use to load credentials
+    // from the shared config file. (~/.aws/credentials).
     sess, err := session.NewSession(&aws.Config{
         Region: aws.String("us-west-2")},
     )
-
-    // Create CloudTrail client
-    svc := cloudtrail.New(sess)
-
-    resp, err := svc.DescribeTrails(&cloudtrail.DescribeTrailsInput{TrailNameList: nil})
     if err != nil {
-        fmt.Println("Got error calling CreateTrail:")
-        fmt.Println(err.Error())
+        fmt.Println("Error getting session:")
+        fmt.Println(err)
         os.Exit(1)
     }
 
-    fmt.Println("Found",len(resp.TrailList),"trail(s) in", regionName)
-    fmt.Println("")
+    // Create DynamoDB client
+    // and expose HTTP requests/responses
+    svc := dynamodb.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
 
-    for _, trail := range resp.TrailList {
-        fmt.Println("Trail name:  " + *trail.Name)
-        fmt.Println("Bucket name: " + *trail.S3BucketName)
-        fmt.Println("")
-    }
+    // Add "timeoutHeader" header with value of 10
+    svc.Handlers.Send.PushFront(func(r *request.Request) {
+        r.HTTPRequest.Header.Set("timeoutHeader", fmt.Sprintf("%d", 10))
+    })
+
+    // Call ListTables just to see HTTP request/response
+    // The request should have the timeoutHeader set to 10
+    _, _ = svc.ListTables(&dynamodb.ListTablesInput{})
 }
